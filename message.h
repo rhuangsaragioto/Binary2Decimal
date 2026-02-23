51b9f274b68891372e2c5c5f2c024ee9c3d328bf
rhuangsaragioto <rhuangsaragioto10@gmail.com>
2026-02-23 07:47:15 -0500

    Fixed formatting of messages

--- message.h
+++ message.h
@@ -6,8 +6,8 @@
 #include <string_view>
 
 const std::string_view NO_INPUT = "You need to pass in a binary number!\n";
-const std::string_view WRONG_INPUT = "Entered input must be a binary number!\n";
+const std::string_view WRONG_INPUT = "Entered input MUST be a binary number!\n";
 const std::string_view USAGE = "Usage: ./Bin2Dec binary_number"
-                               "  binary_number - a string containing only 1s and 0s. i.e., '1010001'";
+                               " | binary_number - a string containing only 1s and 0s. i.e., '1010001'";
 
 #endif
