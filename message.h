998402ebbf0a0a58a2e7f1e158dd82cecb999c39
rhuangsaragioto <rhuangsaragioto10@gmail.com>
2026-02-22 20:46:04 -0500

    updates messages

--- message.h
+++ message.h
@@ -5,7 +5,9 @@
 
 #include <string_view>
 
-const std::string_view HELP_MESSAGE = "";
-const std::string_view WELCOME_MESSAGE = "";
+const std::string_view NO_INPUT = "You need to pass in a binary number!\n";
+const std::string_view WRONG_INPUT = "Entered input must be a binary number!\n";
+const std::string_view USAGE = "Usage: ./Bin2Dec binary_number"
+                               "  binary_number - a string containing only 1s and 0s. i.e., '1010001'";
 
 #endif
