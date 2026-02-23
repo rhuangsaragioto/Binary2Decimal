d08518a921ce46bfd09cb7ed88cbfe1cf1a74cf0
rhuangsaragioto <rhuangsaragioto10@gmail.com>
2026-02-22 20:46:16 -0500

    fixed conversion

--- Bin2Dec.cpp
+++ Bin2Dec.cpp
@@ -4,21 +4,42 @@
 #include <cmath>
 #include <string>
 
+double binToDec(std::string binary) {
+    double decimal = 0;
+    size_t exp = binary.size() - 1;
+    for (size_t i = 0; i < binary.size(); i++) {
+        decimal += std::pow(2, exp) * static_cast<double>(binary[i] - '0');
+        if (exp > 0)
+            exp--;
+        else
+            exp = 0;
+    }
+    return decimal;
+}
+
+bool checkIfBinary(std::string input) {
+    for (size_t i = 0; i < input.size(); i++) {
+        if ((input[i] != '0') || (input[i] == '1'))
+            return false;
+    }
+    return true;
+}
+
 int main(int argc, const char* argv[]) {
     std::string binary;
     if (argc == 1) {
-        std::cout << "You need to pass in a binary number! See -u for usage." << std::endl;
+        std::cout << NO_INPUT << USAGE << std::endl;
         return -1;
     } else {
-        binary = argv[1];
+        if (checkIfBinary(argv[1]) == 1)
+            binary = argv[1];
+        else {
+            std::cout << WRONG_INPUT << USAGE << std::endl;
+        }
     }
 
-    size_t decimal = 0;
-    int exp = binary.size() - 1;
-    for (size_t i = 0; i < binary.size(); i++) {
-        decimal += std::pow(2, exp) * static_cast<int>(binary[i] - '0');
-    }
-
-    std::cout << decimal << std::endl;
+    double decimal = binToDec(binary);
+    std::cout << "Binary: " << binary << std::endl;
+    std::cout << "Decimal: " << decimal << std::endl;
     return 0;
 }
